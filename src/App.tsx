c6cd0c24e78c2428af7d0902bd1f1d465e0b5ae8
Tom Dooner <tomdooner@gmail.com>
2019-01-05 19:51:41 -0800

    Add initial OAuth2 implicit flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ClientOAuth2 from 'client-oauth2';
 import './App.css';
 
+const githubAuth = new ClientOAuth2({
+  clientId: '910420dba3f7db2cc4e0',
+  authorizationUri: 'https://github.com/login/oauth/authorize',
+  redirectUri: 'http://localhost/oauth/callback',
+  scopes: ['user:email'],
+});
+
 class App extends Component {
+  handleLoginClick() {
+    window.location.href = githubAuth.token.getUri();
+  }
+
   render() {
     return (
       <div className="App">
-        Log in with oauth
+        <a href='#' onClick={this.handleLoginClick}>Log in with oauth</a>
       </div>
     );
   }
